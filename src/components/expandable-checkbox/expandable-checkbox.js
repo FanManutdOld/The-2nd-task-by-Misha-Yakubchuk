73e439498fe80308f8145ee917ad45a74d0c6851
FanManutd <maykl.yakubchuk@mail.ru>
2020-02-21 17:26:32 +0300

    A little refactoring

--- src/components/expandable-checkbox/expandable-checkbox.js
+++ src/components/expandable-checkbox/expandable-checkbox.js
@@ -1,9 +1,11 @@
-console.log('hello from expandable');
 const checklists = document.querySelectorAll('.checkbox-list');
-let add_mod_expanded = function (checklist) {
+
+
+let toggleExpandedMod = function (checklist) {
   const checkwrapper = checklist.querySelector('.checkbox-list__wrapper');
   const checkarrow = checkwrapper.querySelector('.checkbox-list__arrow');
   const checkcheckboxes = checklist.querySelector('.checkbox-list__checkboxes');
+  
   checkwrapper.addEventListener('click', function(){
     checkarrow.classList.toggle('checkbox-list__arrow_expanded');
     checkcheckboxes.classList.toggle('checkbox-list__checkboxes_expanded');
@@ -12,5 +14,5 @@ let add_mod_expanded = function (checklist) {
 
 for(let i = 0; i < checklists.length; i++)
 {
-  add_mod_expanded(checklists[i]);
+  toggleExpandedMod(checklists[i]);
 }
