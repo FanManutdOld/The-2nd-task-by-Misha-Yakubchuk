471f4fe25a85fc7be184475ce7e1eabe8f33185d
FanManutd <maykl.yakubchuk@mail.ru>
2020-03-30 18:54:12 +0300

    Added burger and user-mode to header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,18 +1,58 @@
-class header{
-  constructor (item) {
+class header {
+  constructor(item) {
     this.init(item);
   }
 
   init(header) {
+    this.bindHandleSubmenuClick(header);
+    this.bindHandleBurgerClick(header);
+    this.bindHandleUserClick(header);
+  }
+
+
+  bindHandleSubmenuClick(header) {
     this.submenuArray = header.querySelectorAll('.js-header__nav-item')
     this.submenuArray.forEach(item => {
       const submenu = item.querySelector('.js-header__submenu');
       item.addEventListener('click', this.handleSubmenuClick.bind(this, submenu));
     });
-  } 
+  }
+
+  bindHandleBurgerClick(header) {
+    this.burger = header.querySelector('.js-header__burger');
+    this.navbar = header.querySelector('.js-header__navbar');
+    this.burger.addEventListener('click', this.handleBurgerClick.bind(this));
+  }
+
+  bindHandleUserClick(header) {
+    this.user = header.querySelector('.js-header__user');
+    if(header.querySelector('.js-header__buttons')) {
+      this.userDropdown = header.querySelector('.js-header__buttons');
+      this.userDropdownClass = 'header__buttons_show';
+    } 
+    else {
+      this.userDropdown = header.querySelector('.js-header__username');
+      this.userDropdownClass = 'header__username_show';
+    }
+    this.user.addEventListener('click', this.handleUserClick.bind(this));
+  }
 
   handleSubmenuClick(submenu) {
     submenu.classList.toggle('header__submenu_hidden');
+  }
+
+  handleBurgerClick() {
+    this.navbar.classList.toggle('header__navbar_show');
+    if (this.userDropdown.classList.contains(this.userDropdownClass)) {
+      this.userDropdown.classList.remove(this.userDropdownClass);
+    }
+  }
+
+  handleUserClick() {
+    this.userDropdown.classList.toggle(this.userDropdownClass);
+    if (this.navbar.classList.contains('header__navbar_show')) {
+      this.navbar.classList.remove('header__navbar_show');
+    }
   }
 }
 
