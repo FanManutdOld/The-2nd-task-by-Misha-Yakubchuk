ab373910d9b7a6f3b2c7a82d7a8fc2f8eefdf64a
FanManutd <maykl.yakubchuk@mail.ru>
2020-04-12 20:18:48 +0300

    Finished room-details

--- src/pages/websitePages/room-details/room-details.js
+++ src/pages/websitePages/room-details/room-details.js
@@ -1,4 +1,6 @@
 import header from '../../../components/header/header.js';
+import likeButton from '../../../components/like-button/like-button';
+import bookingRoomCard from '../../../components/booking-room-card/booking-room-card.js';
 
 
 class roomDetails{
@@ -8,11 +10,27 @@ class roomDetails{
 
   init() {
     this.searchHeader();
+    this.searchLikeButtons();
+    this.searchBookingRoomCard();
   }
 
   searchHeader() {
     const headerOnPage = document.querySelector('.header');
     new header(headerOnPage);
+  }
+
+  searchLikeButtons() {
+    const likeButtons = document.querySelectorAll('.js-like-button');
+    
+    //для каждой кнопки на странице
+    likeButtons.forEach(item => {
+      new likeButton(item);
+    });
+  }
+
+  searchBookingRoomCard() {
+    const bookingCard = document.querySelector('.js-booking-room-card');
+    new bookingRoomCard(bookingCard);
   }
 }
 
