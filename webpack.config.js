90c4f41e3481b77d1ba34b9cecbbe672eddb2d57
FanManutd <maykl.yakubchuk@mail.ru>
2020-10-09 21:55:46 +0300

    Webpack in process

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,56 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: '',
+    filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
   },
-  
+  module: {
+    rules: [
+      {
+        test: "/\.s?css$/",
+        use: [
+          MiniCssExtractPlugin.loader, 
+          "css-loader",
+          "sass-loader"
+        ]
+      },
+      {
+        test: "/\.(woff|woff2|ttf|eot|svg)$/",
+        include: [
+          path.resolve(__dirname, 'src/fonts'),
+        ],
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[ext]",
+            outputPath: path.resolve(__dirname, 'assets/image')
+          }
+        }
+      },
+      {
+        test: "/\.(png|jpg|jpeg|svg|gif)$/",
+        exclude: [
+          path.resolve(__dirname, 'src/fonts'),
+        ],
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[ext]",
+            outputPath: path.resolve(__dirname, 'assets/image')
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css"
+    })
+  ],
 };
