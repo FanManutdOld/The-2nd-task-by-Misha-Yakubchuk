db7ec5d3942ecc654fed8f97fce89846402e9e2e
FanManutd <maykl.yakubchuk@mail.ru>
2020-10-11 11:29:07 +0300

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,23 @@
 const path = require('path');
+const fs = require('fs');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
+
+let bufPages; let dirNames = []; let PAGES; const PAGES_DIRS = [];
+bufPages = path.resolve(__dirname, 'src/pages/ui-kit');
+dirNames = fs.readdirSync(bufPages);
+dirNames.forEach((item) => {
+  PAGES_DIRS.push(path.resolve(__dirname, `src/pages/ui-kit/${item}`));
+});
+PAGES = dirNames;
+bufPages = path.resolve(__dirname, 'src/pages/websitePages');
+dirNames = fs.readdirSync(bufPages);
+dirNames.forEach((item) => {
+  PAGES_DIRS.push(path.resolve(__dirname, `src/pages/websitePages/${item}`));
+});
+PAGES = PAGES.concat(dirNames);
 
 module.exports = {
   entry: './src/index.js',
@@ -9,48 +25,76 @@ module.exports = {
     filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
   },
+  devtool: 'source-map',
   module: {
     rules: [
       {
-        test: "/\.s?css$/",
-        use: [
-          MiniCssExtractPlugin.loader, 
-          "css-loader",
-          "sass-loader"
-        ]
+        test: /\.pug$/,
+        use: {
+          loader: 'pug-loader',
+          options: {
+            globals: 'asd',
+          },
+        },
       },
       {
-        test: "/\.(woff|woff2|ttf|eot|svg)$/",
+        test: /\.s?css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.(woff|woff2|ttf|eot|svg)$/,
         include: [
           path.resolve(__dirname, 'src/fonts'),
         ],
         use: {
-          loader: "file-loader",
+          loader: 'file-loader',
           options: {
-            name: "[name].[ext]",
-            outputPath: path.resolve(__dirname, 'assets/image')
-          }
-        }
+            name: '[name].[ext]',
+            outputPath: 'assets/fonts',
+          },
+        },
       },
       {
-        test: "/\.(png|jpg|jpeg|svg|gif)$/",
+        test: /\.(png|jpg|jpeg|svg|gif)$/,
         exclude: [
           path.resolve(__dirname, 'src/fonts'),
         ],
         use: {
-          loader: "file-loader",
+          loader: 'file-loader',
           options: {
-            name: "[name].[ext]",
-            outputPath: path.resolve(__dirname, 'assets/image')
-          }
-        }
-      }
-    ]
+            name: '[name].[ext]',
+            outputPath: 'assets/images',
+          },
+        },
+      },
+    ],
   },
   plugins: [
     new CleanWebpackPlugin(),
+    ...PAGES.map((page, index) => new HtmlWebpackPlugin({
+      filename: `${page}.html`,
+      template: `${PAGES_DIRS[index]}/${page}.pug`,
+    })),
     new MiniCssExtractPlugin({
-      filename: "[name].[contenthash].css"
-    })
+      filename: '[name].[contenthash].css',
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+    }),
   ],
 };
